2ee402188133eb33dcd36305e0f1a85d0a5bc234
flango <daniel.nagami@woopi.com.br>
2020-11-18 22:33:25 -0300

    Finishing Entities

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace PaymentContext.Domain.Entities
 {
     public class Student
@@ -6,5 +8,7 @@ namespace PaymentContext.Domain.Entities
         public string LastName { get; set; }
         public string Document { get; set; }
         public string Email { get; set; }
+        public List<Subscription> Subscriptions { get; set; }
+        public string Address { get; set; }
     }
 }
--- PaymentContext.Domain/Entities/Subscription.cs
+++ PaymentContext.Domain/Entities/Subscription.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace PaymentContext.Domain.Entities
 {
@@ -7,5 +8,7 @@ namespace PaymentContext.Domain.Entities
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
         public DateTime? ExpireDate { get; set; }
+        public bool Active { get; set; }
+        public List<Payment> Payments { get; set; }
     }
 }
