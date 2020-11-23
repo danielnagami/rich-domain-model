ca3785d01aa73080059af4bbfc25e0e49a447442
flango <daniel.nagami@woopi.com.br>
2020-11-23 02:34:59 -0300

    Adding handlers

--- PaymentContext.Domain/Entities/CreditCardPayment.cs
+++ PaymentContext.Domain/Entities/CreditCardPayment.cs
@@ -5,15 +5,15 @@ namespace PaymentContext.Domain.Entities
 {
     public class CreditCardPayment : Payment
     {
-        public CreditCardPayment(string cardHolderName, string carNumber, string lastTransactionNumber, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email) : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
+        public CreditCardPayment(string cardHolderName, string cardNumber, string lastTransactionNumber, DateTime paidDate, DateTime expireDate, decimal total, decimal totalPaid, string payer, Document document, Address address, Email email) : base(paidDate, expireDate, total, totalPaid, payer, document, address, email)
         {
             CardHolderName = cardHolderName;
-            CarNumber = carNumber;
+            CardNumber = cardNumber;
             LastTransactionNumber = lastTransactionNumber;
         }
 
         public string CardHolderName { get; private set; }
-        public string CarNumber { get; private set; }
+        public string CardNumber { get; private set; }
         public string LastTransactionNumber { get; private set; }
     }
 }
